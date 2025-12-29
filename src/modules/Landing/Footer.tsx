71bd54d6ea45caa5ff701aa2c97e5db59bd10cab
Dinuka Dilshan <jadinukadilshan@gmail.com>
2025-12-29 23:03:45 +0530

    Update metadata

--- src/modules/Landing/Footer.tsx
+++ src/modules/Landing/Footer.tsx
@@ -2,7 +2,8 @@ const Footer = () => {
   return (
     <footer className="w-full flex  items-center  pt-16">
       <p className="text-mute-light text-xs">
-        © {new Date().getFullYear()} Dinuka Dilshan. All rights reserved.
+        © {new Date().getFullYear()} Dinuka Abeygunawardhana (Dinuka Dilshan).
+        All rights reserved.
       </p>
     </footer>
   );
